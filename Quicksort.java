2fad0b58a42926befd58e0021f68a677808ff08b
Asif Rahman <93546485+asifrahman03@users.noreply.github.com>
2024-03-31 22:45:25 -0400

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -40,4 +40,37 @@ public class Quicksort {
     public static void main(String[] args) {
         // This is the main file for the program.
     }
+
+    public Quicksort(String filename) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(filename, "r");
+        byte[] some = new byte[(int)file.length()];
+        file.read(some);
+        this.quickSort(some, 0, some.length-1);
+    }
+
+    
+    private void swap(byte[] arr, int indexA, int indexB) {
+        byte[] byteLeft = new byte[4];
+        byte[] byteRight = new byte[4];
+        System.arraycopy(arr, indexA, byteLeft, 0, 4);
+        System.arraycopy(arr, indexB, byteRight, 0, 4);
+        System.arraycopy(byteLeft,0 ,arr, indexB, 4);
+        System.arraycopy(byteRight,0, arr, indexA, 4);
+        //return arr;
+    }
+
+    public void quickSort(byte[] arr, int lowIndex, int highIndex) {
+        long beginning = System.currentTimeMillis(); 
+        int pivotIndex = (highIndex + lowIndex) / 2; //value used for quickSort comparisons
+        
+        swap(arr, pivotIndex, highIndex); //swap it to end of array
+        
+        int partitionIndex = partition(arr, lowIndex, highIndex-1, arr[highIndex]); //beginning position is array minus pivot
+        swap(arr, partitionIndex, highIndex); //places new pivot
+        
+        if((partitionIndex - highIndex) > 1) quickSort(arr, lowIndex, partitionIndex - 1); //sorts left partition
+        if((highIndex - partitionIndex) > 1) quickSort(arr, partitionIndex + 1, highIndex);
+        long end = System.currentTimeMillis();
+        long timeToSort = end - beginning;
+    }
 }
