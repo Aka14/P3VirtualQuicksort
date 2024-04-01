de6391ea1e223a5095e3f35b4f560586eb63adab
Asif Rahman <93546485+asifrahman03@users.noreply.github.com>
2024-04-01 17:36:13 -0400

    Update Quicksort.java

--- Quicksort.java
+++ Quicksort.java
@@ -39,50 +39,22 @@ public class Quicksort {
      */
     public static void main(String[] args) {
         // This is the main file for the program.
-    }
-
-    public Quicksort(String filename) throws IOException {
-        RandomAccessFile file = new RandomAccessFile(filename, "r");
-        byte[] some = new byte[(int)file.length()];
-        file.read(some);
-        this.quickSort(some, 0, some.length-1);
-    }
-
-    
-    private void swap(byte[] arr, int indexA, int indexB) {
-        byte[] byteLeft = new byte[4];
-        byte[] byteRight = new byte[4];
-        System.arraycopy(arr, indexA, byteLeft, 0, 4);
-        System.arraycopy(arr, indexB, byteRight, 0, 4);
-        System.arraycopy(byteLeft,0 ,arr, indexB, 4);
-        System.arraycopy(byteRight,0, arr, indexA, 4);
-        //return arr;
-    }
-
-    public void quickSort(byte[] arr, int lowIndex, int highIndex) {
+        if(args.length != 3) {
+            System.out.println("Invalid parameters, input correct parameters.");
+            return;
+        }
+        File file = new File(args[0]);
+        if(!file.exists()) System.out.println("File does not exist. Please input exisiting file");
+        
+        BufferPool bp = new BufferPool(args[0], Integer.parseInt(args[1]));
+        
+        PrintWriter statFile = new PrintWriter(args[2]);
+        
         long beginning = System.currentTimeMillis(); 
-        int pivotIndex = (highIndex + lowIndex) / 2; //value used for quickSort comparisons
-        
-        swap(arr, pivotIndex, highIndex); //swap it to end of array
-        
-        int partitionIndex = partition(arr, lowIndex, highIndex-1, arr[highIndex]); //beginning position is array minus pivot
-        swap(arr, partitionIndex, highIndex); //places new pivot
-        
-        if((partitionIndex - highIndex) > 1) quickSort(arr, lowIndex, partitionIndex - 1); //sorts left partition
-        if((highIndex - partitionIndex) > 1) quickSort(arr, partitionIndex + 1, highIndex);
+        new SortFunction(args[0], bp);
         long end = System.currentTimeMillis();
         long timeToSort = end - beginning;
+        
     }
-    public int partition(byte[] arr, int lowIndex, int highIndex, short pivot) {
-        ByteBuffer bb = ByteBuffer.wrap(arr);
-        int i = lowIndex - 1;
-        for (int j = lowIndex; j < highIndex; j++) {
-            if (bb.getShort(j) < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, highIndex);
-        return i + 1;
-    }
+
 }
