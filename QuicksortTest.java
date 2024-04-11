6746507f2e0a17a738719c1054f8c00f40946812
Akash Bhukhanwala <41841642+Aka14@users.noreply.github.com>
2024-04-10 20:19:11 -0400

    Update QuicksortTest.java

--- QuicksortTest.java
+++ QuicksortTest.java
@@ -37,22 +37,22 @@ public class QuicksortTest extends TestCase {
     }
     
     
-    public void testCheckFile() throws Exception {
-        assertTrue(CheckFile.check("tinySorted.txt"));
-        
-        String fname = "checkme.txt";
-        FileGenerator fg = new FileGenerator(fname, 1);
-        fg.setSeed(42);
-        fg.generateFile(FileType.ASCII);
-        // Notice we *re-generate* this file each time the test runs.
-        // That file persists after the test is over
-        
-        assertFalse(CheckFile.check(fname));
-        
-        // hmmm... maybe do some sorting on that file ... 
-        // then we can do:
-        // assertTrue(CheckFile.check(fname));
-    }
+//    public void testCheckFile() throws Exception {
+//        assertTrue(CheckFile.check("tinySorted.txt"));
+//        
+//        String fname = "checkme.txt";
+//        FileGenerator fg = new FileGenerator(fname, 1);
+//        fg.setSeed(42);
+//        fg.generateFile(FileType.ASCII);
+//        // Notice we *re-generate* this file each time the test runs.
+//        // That file persists after the test is over
+//        
+//        assertFalse(CheckFile.check(fname));
+//        
+//        // hmmm... maybe do some sorting on that file ... 
+//        // then we can do:
+//        // assertTrue(CheckFile.check(fname));
+//    }
     
 
     /** This method is a demonstration of the file generator and file checker
@@ -61,22 +61,56 @@ public class QuicksortTest extends TestCase {
      * we don't call a sort method in this test, so we assertFalse).
      *
      * @throws Exception either a IOException or FileNotFoundException */
-    public void testSorting() throws Exception {
-        String fname = "input.bin";
-        FileGenerator fg = new FileGenerator(fname, 1);
-        fg.generateFile(FileType.BINARY);
-        
-        assertFalse(CheckFile.check(fname)); // file shouldn't be sorted
+//    public void testSorting() throws Exception {
+//        String fname = "input.bin";
+//        FileGenerator fg = new FileGenerator(fname, 1);
+//        fg.generateFile(FileType.BINARY);
+//        
+//        assertFalse(CheckFile.check(fname)); // file shouldn't be sorted
+//
+//        String[] args = new String[3];
+//        args[0] = fname; // the file to be sorted.
+//        args[1] = "1"; // number of buffers, can impact performance
+//        args[2] = "stats.txt"; // filename for sorting stats
+//        Quicksort.main(args);
+//        // Now the file *should* be sorted, so lets check!
+//
+//        // TODO: In a real test, the following should work:
+//        // assertTrue(CheckFile.check(fname));
+//    }
+    
+//public void testSort() throws Exception{
+//        
+//        FileGenerator fg = new FileGenerator("OneBlockASCIIOneBuffer.txt", 2);
+//        fg.generateFile(FileType.ASCII);
+//        
+//        assertFalse(CheckFile.check("OneBlockASCIIOneBuffer.txt"));
+//        
+//        String[] listS = new String[3];
+//        listS[0] = "OneBlockASCIIOneBuffer.txt";
+//        listS[1] = "2";
+//        listS[2] = "stats_OneBlockASCIIOneBuffer.txt";
+//        Quicksort.main(listS);
+//        
+//        assertTrue(CheckFile.check("OneBlockASCIIOneBuffer.txt"));
+//    }
 
-        String[] args = new String[3];
-        args[0] = fname; // the file to be sorted.
-        args[1] = "1"; // number of buffers, can impact performance
-        args[2] = "stats.txt"; // filename for sorting stats
-        Quicksort.main(args);
-        // Now the file *should* be sorted, so lets check!
-
-        // TODO: In a real test, the following should work:
-        // assertTrue(CheckFile.check(fname));
-    }
+public void testTenBlockASCIITenBuff() throws Exception{
+    
+    FileGenerator fg = new FileGenerator("TenBlockASCIITenBuff.txt", 10);
+    fg.generateFile(FileType.ASCII);
+    
+    assertFalse(CheckFile.check("TenBlockASCIITenBuff.txt"));
+    
+    String[] listS = new String[3];
+    listS[0] = "TenBlockASCIITenBuff.txt";
+    listS[1] = "4";
+    listS[2] = "stats_TenBlockASCIITenBuff.txt";
+    Quicksort.main(listS);
+    
+    assertTrue(CheckFile.check("TenBlockASCIITenBuff.txt"));
+}
+    
+    
 
 }
